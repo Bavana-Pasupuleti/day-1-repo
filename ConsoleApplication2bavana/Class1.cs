4d42d6e6854287e3041be8d2ef48f0fa0197755d
Bavana-Pasupuleti <bavana2712@gmail.com>
2022-01-31 17:11:39 +0530

    second commit

---
+++ ConsoleApplication2bavana/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2bavana
+{
+    class Class1
+    {
+        public void sum()
+        {
+            int a = 10, b = 5;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ConsoleApplication2bavana/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2bavana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //equality program
+            int a = 10, b = 12;
+            String d = a == b ? "equal" : "not equal";
+            //leap year program
+            int year = int.Parse(Console.ReadLine());
+            String c = year%400==0 ? "leap year" :year%100==0?"not leap":year % 4==0 ? "leap" : "not leap";
+            Console.WriteLine(c);
+            Console.ReadLine();
+            
+        }
+    }
+}
